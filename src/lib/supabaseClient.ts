832c6a174d6ed366cfb7cb182ada19c956e11c0c
piedie <piedie@piedie.net>
2025-06-21 20:47:46 +0200

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,1 +1,2 @@
 // ... zie Supabase client hierboven
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsYnd3bXFmbmxndGVjaHlxZmx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NTU2MTMsImV4cCI6MjA2NjAzMTYxM30.STF8sDIXyXOlMVI4bUO97rD_SaT0QIcboQR621yep4E";
